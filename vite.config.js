a70288497ba07f83342951276c0b61ae3e6c42e7
Kern <kerntallett@gmail.com>
2025-10-03 22:26:02 +0100

    Deployed to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/planen/',
 
   // Tauri expects a fixed port, fail if that port is not available
   server: {
